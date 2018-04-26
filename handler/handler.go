7a526bf71914512b71c6999c23c777d7a9409cb8
KimMachineGun <geon0250@gmail.com>
2018-04-26 19:36:46 +0900

    Delete custom error handler

--- handler/handler.go
+++ handler/handler.go
@@ -3,10 +3,8 @@ package handler
 import (
 	"crypto/rand"
 	"io"
-	"net/http"
 
 	"github.com/jinzhu/gorm"
-	"github.com/labstack/echo"
 )
 
 type Handler struct {
@@ -20,36 +18,4 @@ func GenerateRandomKey(length int) []byte {
 		return nil
 	}
 	return k
-}
-
-func (h Handler) HTTPErrorHandler(err error, c echo.Context) {
-	var (
-		code = http.StatusInternalServerError
-		msg  interface{}
-	)
-
-	if he, ok := err.(*echo.HTTPError); ok {
-		code = he.Code
-		msg = he.Message
-	} else {
-		msg = http.StatusText(code)
-	}
-
-	if _, ok := msg.(string); ok {
-		msg = echo.Map{"message": msg}
-	}
-
-	c.Logger().Error(err)
-
-	// Send response
-	if !c.Response().Committed {
-		if c.Request().Method == echo.HEAD { // Issue #608
-			err = c.NoContent(code)
-		} else {
-			err = c.JSON(code, msg)
-		}
-		if err != nil {
-			c.Logger().Error(err)
-		}
-	}
 }
